185584757c16c67710cd9452c813412861102b46
Dinica Dixie Granadel <granadeldanicadixie@gmail.com>
2024-10-03 10:36:08 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+
+{
+	
+	cout << "Our Lady of Lourdes College Foundation" << endl;
+	cout << "College of Computer Studies" << endl;
+	cout << "Bachelor of Science in Information Technology-1st Year" << endl;
+	cout << "Danica Dixie Bual Granadel" << endl;
+	cout << "Purok-2 Anameam Labo Camarines Norte" << endl;
+	
+return 0;
+
+}
