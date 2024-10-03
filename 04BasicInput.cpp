806b961eb735a6a459584a44a405599d27bba77b
Dinica Dixie Granadel <granadeldanicadixie@gmail.com>
2024-10-03 11:43:25 +0800

    Add files via upload

---
+++ 04BasicInput.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+
+{
+	string nickname;
+	string course;
+	string school;
+	
+	cout << "Nickname:";
+	cin >> nickname;
+	cout << "Course-Year Level:";
+	cin >> course;
+	cout << "School:";
+	cin >> school;
+	cout << "Wow Congrats" << nickname << << course << "is a nice course. And you are studying in"
+	
+return 0;
+
+}
