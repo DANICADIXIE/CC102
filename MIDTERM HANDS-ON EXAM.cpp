f51ca327af3f48743322cf456256231ea0608f8b
Danica Dixie Granadel <granadeldanicadixie@gmail.com>
2024-10-15 11:31:37 +0800

    Update MIDTERM HANDS-ON EXAM.cpp

--- MIDTERM HANDS-ON EXAM.cpp
+++ MIDTERM HANDS-ON EXAM.cpp
@@ -4,31 +4,35 @@ using namespace std;
 
 int main ()
 {
-	float sugarPriceUSD, ricePricePound, sardinesPricePound, coffeePriceUSD, milkPriceUSD;
-    float sugarQty, riceQty, sardinesQty, coffeeQty, milkQty;
-    float usdToPhp, poundToPhp;
+	float sugarPriceUSD, coffeePriceUSD, milkPriceUSD;
+	float ricePricePound, sardinesPricePound;
+        float sugarQty, riceQty, sardinesQty, coffeeQty, milkQty;
+        float sugarCostPHP, coffeeCostPHP; milkCostPHP;
+	float usdToPhp = $7.50/1;
 	
-	cout << "Sugar $:\t";
-	cout << "Sugar Quantity:";
-	cin >> sugarQty;
+	cout << "Enter Price of Sugar($)\t\t\t|\t";
+	cin >> sugarPriceUSD;
+	cout << "Enter Price of Rice(Pound)\t\t|\t";
+	cin >> ricePricePound;
+	cout << "Enter Price of Sardines(Pound)\t|\t";
+	cin >> sardinesPricePound;
+	cout << "Enter Price of Coffee ($)\t\t|\t";
+	cin >> coffeePriceUSD;
+	cout << "Enter Price of Coffee ($)\t\t|\t";
+	cin >>  milkPriceUSD;
 	
-	cout << "Rice $:\t";
-	cout << "Rice Quantity:";
-	cin >> riceQty;
-	
-	cout << "Sardines $:\t";
-	cout << "Sardines Quantity:";
-	cin >> sardinesQty;
-	
-	cout << "Coffee $:\t";
-	cout << "Coffee Quantity:";
-	cin >> coffeeQty;
-	
-	cout << "Milk $:\t";
-	cout << "Milk Quantity:";
-	cin >> milkQty;
-	
-	cout << 
+	cout << endl;
+
+	cout << "Enter Sugar Quantity\t\t\t|\t";
+	cin >> sugarPriceUSD;
+	cout << "Enter Rice Quantity\t\t\t|\t";
+	cin >> ricePricePound;
+	cout << "Enter Sardines Quantity\t\t|\t";
+	cin >> sardinesPricePound;
+	cout << "Enter Price of Coffee ($)\t\t|\t";
+	cin >> coffeePriceUSD;
+	cout << "Enter Price of Coffee ($)\t\t|\t";
+	cin >>  milkPriceUSD;
 	
     
 	
