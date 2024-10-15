10ad1f78a285818a8cb121753fe6c16602173f3f
Danica Dixie Granadel <granadeldanicadixie@gmail.com>
2024-10-15 08:47:53 +0800

    Add files via upload

---
+++ MIDTERM HANDS-ON EXAM.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	float sugarPriceUSD, ricePricePound, sardinesPricePound, coffeePriceUSD, milkPriceUSD;
+    float sugarQty, riceQty, sardinesQty, coffeeQty, milkQty;
+    float usdToPhp, poundToPhp;
+	
+	cout << "Sugar $:\t";
+	cout << "Sugar Quantity:";
+	cin >> sugarQty;
+	
+	cout << "Rice $:\t";
+	cout << "Rice Quantity:";
+	cin >> riceQty;
+	
+	cout << "Sardines $:\t";
+	cout << "Sardines Quantity:";
+	cin >> sardinesQty;
+	
+	cout << "Coffee $:\t";
+	cout << "Coffee Quantity:";
+	cin >> coffeeQty;
+	
+	cout << "Milk $:\t";
+	cout << "Milk Quantity:";
+	cin >> milkQty;
+	
+	cout << 
+	
+    
+	
+
+
+
+
+
+
+
+}
